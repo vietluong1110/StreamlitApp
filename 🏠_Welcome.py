e3b3902fee0272ae96174d40a61b17c7effbeff9
vietluong1110 <106910033+vietluong1110@users.noreply.github.com>
2023-05-07 15:17:42 +0700

    Add files via upload

---
+++ 🏠_Welcome.py
@@ -0,0 +1,9 @@
+#Import packages
+import streamlit as st
+
+#Set Page Config 
+st.set_page_config(page_icon="🤖", page_title="LLMs Assistant")
+#Set Page Title
+st.title('🤖 Your LLMs Assistant')
+#Create a sidebar
+st.sidebar.success("Navigator")
